51100242df1fffe9d8c871671fd4dad1fecef437
dethof.alexander <alexander.dethof@telekom.de>
2016-06-28 16:06:54 +0200

    corrected dynamic content payload type max value

--- bitstreamparse/rtp/rawVideo.py
+++ bitstreamparse/rtp/rawVideo.py
@@ -6,7 +6,7 @@ from bitstreamparse.rtpParser import RtpParser
 class RawVideo(RtpParser):
 
     DYNAMIC_PAYLOAD_TYPE_ID_MIN = 96
-    DYNAMIC_PAYLOAD_TYPE_ID_MAX = 96
+    DYNAMIC_PAYLOAD_TYPE_ID_MAX = 127
 
     def _is_valid_payload_type(self, payload_type):
         assert isinstance(payload_type, int), 'Invalid payload type given!'
